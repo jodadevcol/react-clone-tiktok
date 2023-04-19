08d1bc486843b061ed30c7b7acc12aa7f166d6cb
Daniel Garcia (jodadev.col) <jodadev.col@gmail.com>
2023-04-19 22:01:30 +1000

    build: build project base

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App () {
+  return (
+    <div className="App">
+      <main id='main-content'>
+        <section className="bl-content"></section>
+      </main>
+    </div>
+  )
+}
+
+export default App
